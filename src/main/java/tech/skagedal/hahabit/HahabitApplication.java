41c5e346ec2defe1f29798c63eb66a5ba3eb9147
Simon Kågedal Reimer <skagedal@gmail.com>
2023-01-01 20:18:09 +0100

    Add Spring Boot skeleton (straight out of Spring Initializr)

---
+++ src/main/java/tech/skagedal/hahabit/HahabitApplication.java
@@ -0,0 +1,13 @@
+package tech.skagedal.hahabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HahabitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HahabitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/skagedal/hahabit/HahabitApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.skagedal.hahabit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HahabitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
