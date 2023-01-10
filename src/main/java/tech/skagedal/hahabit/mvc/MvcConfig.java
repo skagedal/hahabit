79aad4b216df6e9cf91ae9512201e67535aaf83e
Simon Kågedal Reimer <skagedal@gmail.com>
2023-01-10 18:23:43 +0100

    Add link to habits page + view controller

--- src/main/java/tech/skagedal/hahabit/mvc/MvcConfig.java
+++ src/main/java/tech/skagedal/hahabit/mvc/MvcConfig.java
@@ -9,6 +9,6 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/habits").setViewName("habits");
     }
 }
