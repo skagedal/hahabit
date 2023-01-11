94ea3729936a69787d012c4745f0cf9d87bd30be
Simon Kågedal Reimer <skagedal@gmail.com>
2023-01-11 14:56:04 +0100

    Hook up HabitRepository to HabitController

--- src/main/java/tech/skagedal/hahabit/mvc/HabitsController.java
+++ src/main/java/tech/skagedal/hahabit/mvc/HabitsController.java
@@ -1,24 +1,27 @@
 package tech.skagedal.hahabit.mvc;
 
-import java.util.List;
+import java.security.Principal;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-import tech.skagedal.hahabit.model.Habit;
+import tech.skagedal.hahabit.repository.HabitRepository;
 
 @Controller
 public class HabitsController {
+    private final HabitRepository habits;
+
+    public HabitsController(HabitRepository habits) {
+        this.habits = habits;
+    }
+
     @GetMapping("/habits")
-    ModelAndView getHabits() {
+    ModelAndView getHabits(Principal principal) {
         return new ModelAndView(
             "habits",
             Map.of(
-                "habits", List.of(
-                    Habit.create("simon", "Do things"),
-                    Habit.create("simon", "Do other things")
-                )
+                "habits",
+                habits.findAllByOwnedBy(principal.getName())
             )
         );
     }
--- src/main/java/tech/skagedal/hahabit/repository/HabitRepository.java
+++ src/main/java/tech/skagedal/hahabit/repository/HabitRepository.java
@@ -1,7 +1,9 @@
 package tech.skagedal.hahabit.repository;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import tech.skagedal.hahabit.model.Habit;
 
 public interface HabitRepository extends CrudRepository<Habit, Long> {
+    List<Habit> findAllByOwnedBy(String user);
 }
