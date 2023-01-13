126dc5b83045e2f55e3674bb8b266de39f35a912
Simon Kågedal Reimer <skagedal@gmail.com>
2023-01-13 19:11:50 +0100

    Naive implementation of HomeController getting habits by date

--- src/main/java/tech/skagedal/hahabit/mvc/HomeController.java
+++ src/main/java/tech/skagedal/hahabit/mvc/HomeController.java
@@ -7,21 +7,46 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import tech.skagedal.hahabit.model.Achievement;
 import tech.skagedal.hahabit.model.HabitForDate;
+import tech.skagedal.hahabit.repository.AchievementRepository;
+import tech.skagedal.hahabit.repository.HabitRepository;
 
 @Controller
 public class HomeController {
+    private final HabitRepository habits;
+    private final AchievementRepository achievements;
+
+    public HomeController(HabitRepository habits, AchievementRepository achievements) {
+        this.habits = habits;
+        this.achievements = achievements;
+    }
+
     @GetMapping("/")
     ModelAndView getHome(Principal principal) {
         return new ModelAndView(
             "home",
             Map.of(
-                "date", "2023-01-16",
-                "habits", List.of(
-                    new HabitForDate(1L, "Eat breakfast", LocalDate.now(), 1L),
-                    new HabitForDate(2L, "Shower", LocalDate.now(), null)
+                "date", "2023-01-13",
+                "habits", getHabitsForDate(
+                    principal,
+                    LocalDate.of(2023, 1, 13)
                 )
             )
         );
+    }
+
+    private List<HabitForDate> getHabitsForDate(Principal principal, LocalDate date) {
+        return habits.findAllByOwnedBy(principal.getName())
+            .stream()
+            .map(habit -> new HabitForDate(
+                habit.id(),
+                habit.description(),
+                date,
+                achievements.findOneByAchievingHabitAndDate(habit.id(), date)
+                    .map(Achievement::id)
+                    .orElse(null)
+            ))
+            .toList();
     }
 }
--- src/main/java/tech/skagedal/hahabit/repository/AchievementRepository.java
+++ src/main/java/tech/skagedal/hahabit/repository/AchievementRepository.java
@@ -1,10 +1,13 @@
 package tech.skagedal.hahabit.repository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import tech.skagedal.hahabit.model.Achievement;
 import tech.skagedal.hahabit.model.Habit;
 
 public interface AchievementRepository extends CrudRepository<Achievement, Long> {
     List<Achievement> findAllByAchievingHabit(Long habitId);
+    Optional<Achievement> findOneByAchievingHabitAndDate(Long id, LocalDate date);
 }
