21540857be4c630ac30fadf60ee05dec880dcfbf
Simon Kågedal Reimer <skagedal@gmail.com>
2023-01-16 10:04:00 +0100

    Add ACL checking for achieving habits

--- src/main/java/tech/skagedal/hahabit/mvc/HomeController.java
+++ src/main/java/tech/skagedal/hahabit/mvc/HomeController.java
@@ -5,22 +5,27 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
+import tech.skagedal.hahabit.model.Achievement;
 import tech.skagedal.hahabit.model.HabitForDate;
+import tech.skagedal.hahabit.repository.AchievementRepository;
 import tech.skagedal.hahabit.repository.HabitRepository;
 
 @Controller
 public class HomeController {
     private final HabitRepository habits;
+    private final AchievementRepository achievements;
 
-    public HomeController(HabitRepository habits) {
+    public HomeController(HabitRepository habits, AchievementRepository achievements) {
         this.habits = habits;
+        this.achievements = achievements;
     }
 
     @GetMapping("/")
@@ -36,12 +41,24 @@ public class HomeController {
         );
     }
 
-    private record AchieveForm(String habitId, LocalDate date) {}
+    private record AchieveForm(Long habitId, LocalDate date) {}
 
-    @PostMapping("/achieve/{habitId}/{date}/achieve")
+    @PostMapping("/habit/{habitId}/{date}/achieve")
     ModelAndView achieve(Principal principal, AchieveForm achieveForm) {
-        System.out.println("Achieve: " + achieveForm);
+        if (!userOwnsHabitWithId(principal.getName(), achieveForm.habitId())) {
+            throw new AccessDeniedException("Unknown habit");
+        }
+        achievements.save(Achievement.create(
+            achieveForm.date(),
+            achieveForm.habitId()
+        ));
         return new ModelAndView(new RedirectView("/"));
+    }
+
+    private boolean userOwnsHabitWithId(String userName, Long habitId) {
+        return habits.findById(habitId)
+            .map(habit -> Objects.equals(habit.ownedBy(), userName))
+            .orElse(false);
     }
 
     private List<HabitForDate> getHabitsForDate(Principal principal, LocalDate date) {
