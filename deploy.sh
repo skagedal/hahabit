ebcf38f0f1fc8fa67aef1512f36c033cb1dc8f26
Simon Kågedal Reimer <skagedal@gmail.com>
2023-02-12 15:38:55 +0100

    Add Spring Boot Actuator and wait for right version to deploy

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,34 @@
 # Exit on errors
 set -e
 
-echo 👋 Building JAR with Java 19...
+if [ -n "$(git status --porcelain)" ]; then
+    echo "🚨 Git repo is dirty, aborting"
+    exit 1
+fi
+
+echo "👋 Building JAR with Java 19..."
 export JAVA_HOME=$(/usr/libexec/java_home -v 19)
 ./gradlew clean bootJar
 
 echo
-echo 👋 Uploading JAR and run script to skagedal.tech...
+echo "👋 Uploading JAR and run script to skagedaal.tech..."
 scp -i ~/.ssh/hahabit-key build/libs/hahabit-0.0.1-SNAPSHOT.jar hahabit@skagedal.tech: 
 scp -i ~/.ssh/hahabit-key server-scripts/run.sh hahabit@skagedal.tech:
 
 echo
-echo 👋 Sending SIGTERM to running service...
+echo "👋 Sending SIGTERM to running service..."
 ssh -i ~/.ssh/hahabit-key hahabit@skagedal.tech pkill -TERM -f hahabit-0.0.1
+
+echo
+echo "👋 Waiting for commit $(git rev-parse --short HEAD) to come up..."
+while true; do
+    sleep 2
+    if [ "$(curl -s https://hahabit.skagedal.tech/actuator/info | jq -r .git.commit.id)" = "$(git rev-parse --short HEAD)" ]; then
+        echo "Done!"
+        exit 0
+    else
+        echo "Waiting..."
+    fi
+done
+
+
--- src/main/java/tech/skagedal/hahabit/WebSecurityConfig.java
+++ src/main/java/tech/skagedal/hahabit/WebSecurityConfig.java
@@ -3,9 +3,12 @@ package tech.skagedal.hahabit;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -13,5 +16,17 @@ public class WebSecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
         return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(requests -> requests
+                .requestMatchers("/actuator/*").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(Customizer.withDefaults());
+
+        return http.build();
     }
 }
