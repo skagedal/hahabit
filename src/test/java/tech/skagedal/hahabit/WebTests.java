aac897a02090b4a730c6333db11bc7927d3092a9
Simon Kågedal Reimer <skagedal@gmail.com>
2023-02-16 21:50:47 +0100

    Add some helpers and a test that home redirects to login

--- src/test/java/tech/skagedal/hahabit/WebTests.java
+++ src/test/java/tech/skagedal/hahabit/WebTests.java
@@ -1,20 +1,63 @@
 package tech.skagedal.hahabit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import org.assertj.core.api.AbstractStringAssert;
+import org.assertj.core.api.AssertFactory;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import tech.skagedal.hahabit.testing.Containers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class WebTests {
+    @Autowired
+    private ServletWebServerApplicationContext servletContext;
+
+    private final HttpClient httpClient = HttpClient.newBuilder()
+        .version(HttpClient.Version.HTTP_1_1)
+        .build();
+
     @DynamicPropertySource
     static void registerPostgreSQLProperties(DynamicPropertyRegistry registry) {
         Containers.registerDynamicProperties(registry);
     }
 
     @Test
-    void adding_a_habit_and_marking_it_as_achieved() {
+    void home_redirects_to_login() {
+        final var response = send(GET(uri("/")).build());
 
+        assertThat(response.statusCode())
+            .isEqualTo(HttpStatus.FOUND.value()); // that's a 302 redirect
+        assertThat(response.headers().firstValue("Location"))
+            .isPresent()
+            .hasValue(uri("/login").toString());
+    }
+
+    // Helpers
+
+    private URI uri(String path) {
+        return URI.create("http://127.0.0.1:" + servletContext.getWebServer().getPort() + path);
+    }
+
+    private HttpResponse<String> send(HttpRequest request) {
+        try {
+            return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static HttpRequest.Builder GET(URI uri) {
+        return HttpRequest.newBuilder(uri).GET();
     }
 }
