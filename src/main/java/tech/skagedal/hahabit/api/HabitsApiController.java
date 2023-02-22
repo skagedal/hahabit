7011ffa6b99716a8488523d5d81271f336e173e6
Simon Kågedal Reimer <skagedal@gmail.com>
2023-02-22 09:13:07 +0100

    Add handler for /api/habits

--- src/main/java/tech/skagedal/hahabit/api/HabitsApiController.java
+++ src/main/java/tech/skagedal/hahabit/api/HabitsApiController.java
@@ -1,7 +1,9 @@
 package tech.skagedal.hahabit.api;
 
 import java.security.Principal;
+import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,6 +18,16 @@ public class HabitsApiController {
     public HabitsApiController(HabitRepository habits) {
         this.habits = habits;
     }
+
+    @GetMapping("/api/habits")
+    ListHabitsResponse listHabits(Principal principal) {
+        return new ListHabitsResponse(
+            habits.findAllByOwnedBy(principal.getName())
+        );
+    }
+
+    private record ListHabitsResponse(List<Habit> habits) { }
+
 
     @PostMapping("/api/habits")
     @ResponseStatus(HttpStatus.CREATED)
