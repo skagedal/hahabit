f79a86fbbd3b7a658e4e210138ab15cac637c297
Simon Kågedal Reimer <skagedal@gmail.com>
2023-02-25 18:27:36 +0100

    Reformat a file

--- src/main/java/tech/skagedal/hahabit/HahabitApplication.java
+++ src/main/java/tech/skagedal/hahabit/HahabitApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HahabitApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HahabitApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HahabitApplication.class, args);
+    }
 }
